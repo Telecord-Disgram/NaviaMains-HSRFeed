1efc60c4a7a3b66e05e2d19db35084c40c46e8ac
SimpNick6703 <harshluha05012003@gmail.com>
2025-10-01 14:34:21 +0530

    refactor: Added environment secrets support
    	modified:   config.py

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
+import os
 import random
+import dotenv
 
+dotenv.load_dotenv()
 # Add channels' links under quotes in the following format. 
-Channels = [
-    "https://t.me/randomchannel",
-    "https://t.me/anotherchannel",
-    "https://t.me/yetanotherchannel"
-]
+Channels = os.getenv("TELEGRAM_CHANNELS").split(",")
 COOLDOWN = 300 # Strongly recommended to keep more than 5-20s in the long run to avoid being IP banned by Telegram. 
-EMBED_COLOR = int(f"0x{random.randint(0, 0xFFFFFF):06x}", 16) # int(f"0x89a7d9", 16)
+EMBED_COLOR = os.getenv("EMBED_COLOR") if os.getenv("EMBED_COLOR") else int(f"0x{random.randint(0, 0xFFFFFF):06x}", 16) # int(f"0x89a7d9", 16)
 ERROR_PLACEHOLDER = f"Unable to parse this message. Try heading to the message link leading to preview page or Telegram." # Placeholder for unparseable messages 
-WEBHOOK_URL = "https://discord.com/api/webhooks/{webhookID}/{webhookToken}" # Replace this with your Discord webhook, webhookID and webhookToken are 19 and 68 characters long respectively.
-THREAD_ID = None # THREAD_ID = "1234567890987654321" 
+WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL") # Replace this with your Discord webhook, webhookID and webhookToken are 19 and 68 characters long respectively.
+THREAD_ID = os.getenv("DISCORD_THREAD_ID") if os.getenv("DISCORD_THREAD_ID") else None
